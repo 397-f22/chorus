0d0f51fbdcb2a32a75b23c820a63e1dbc105183e
Cynthia <Liangshao2023@u.northwestern.edu>
2022-11-15 16:12:36 -0600

    add drum loop

--- src/utilities/PlayNote.jsx
+++ src/utilities/PlayNote.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import MIDISounds from 'midi-sounds-react';
+import { emptyBeatArray } from "./test.js"
 
 export const PlayNote = ({}) => {
+	const [loop, setLoop] = useState(emptyBeatArray);
 	const instruments = [4, 318, 387, 258, 619, 628, 816]
 	const mappings = {
 		"a": 60,
@@ -16,14 +18,36 @@ export const PlayNote = ({}) => {
 
 	const playTestInstrument = (key) => {
 		if (midiSounds) {
-			midiSounds.playChordNow(selectedInstrument, [selectedNote], 1);
+			console.log(loop);
+			midiSounds.startPlayLoop(loop, 120, 1/16);
 		}
+	}
+
+	const updateLoop = (i) => {
+		var loopCopy = [...loop];
+		if (loopCopy[i][0].length > 0){
+			loopCopy[i][0] = [];
+		} else {
+			loopCopy[i][0] = [27];
+		}
+		setLoop(loopCopy);
+	}
+
+	const isSelected = (i) => {
+		return loop[i][0].length > 0;
 	}
 
 	return <div>
 		<button onClick={playTestInstrument}>playTestInstrument</button>
+		<div className="row">
+			{loop.map((beat, i) => {
+				return <button style={isSelected(i) ? {color:"red"} : {color:"yellow"}} key={i} onClick={() => {
+					updateLoop(i);
+				}}>{i}</button>
+			})}
+		</div>
 		<div style={{display: "none"}}>
-			<MIDISounds ref={(ref) => (setMidiSounds(ref))} instruments={instruments}/>
+			<MIDISounds ref={(ref) => (setMidiSounds(ref))} drums={[27]}/>
 		</div>
 	</div>	
 };
