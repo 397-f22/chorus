e46fa1314f63894f5f6b89c375b148c28bc683e9
Josh <61065503+joshualevitas@users.noreply.github.com>
2022-11-15 14:21:35 -0600

    more instruments

--- src/utilities/components/NoteButton.jsx
+++ src/utilities/components/NoteButton.jsx
@@ -1,46 +1,63 @@
-// import React, { Component } from 'react';
-// import MIDISounds from 'midi-sounds-react';
-
-
-// const  playTestInstrument = () => {
-//     midiSounds.playChordNow(3, [60], 2.5);
-// }
-
-// const MidiButton = () => {
-    
- 
-//     return (
-//       <div>
-        
-//         <p className="App-intro">Press Play to play instrument sound.</p>
-// 		<p><button onClick={playTestInstrument}>Play</button></p>
-// 		<MIDISounds appElementName="root" instruments={[3]} />	
-//       </div>
-//     );
-  
-// }
-
-// export default MidiButton;
-
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
 import MIDISounds from 'midi-sounds-react';
- 
+
+
 class MidiButton extends Component {
-  playTestInstrument() {
-        this.midiSounds.playChordNow(3, [60], 2.5);
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			selectedInstrument: 4
+			,cached:true
+		};
+	}
+	componentDidMount() {
+		console.log('componentDidMount App');
+		this.setState(this.state);
+	}
+	onSelectInstrument(e){
+		var list=e.target;
+		let n = list.options[list.selectedIndex].getAttribute("value");
+		this.setState({
+			selectedInstrument: n
+			,cached:false
+		});
+		this.midiSounds.cacheInstrument(n);
+		var me=this;
+		this.midiSounds.player.loader.waitLoad(function () {
+			me.setState({
+				selectedInstrument: n
+				,cached:true
+			});
+		});
+	}
+	createSelectItems() {
+		if (this.midiSounds) {
+			if (!(this.items)) {
+				this.items = [];
+				for (let i = 0; i < this.midiSounds.player.loader.instrumentKeys().length; i++) {
+					this.items.push(<option key={i} value={i}>{'' + (i + 0) + '. ' + this.midiSounds.player.loader.instrumentInfo(i).title}</option>);
+				}
+			}
+			return this.items;
+		}
+	}
+	playTestInstrument() {
+		this.midiSounds.playChordNow(this.state.selectedInstrument, [60], 2.5);
+	}
   render() {
     return (
-      <div>
-      
-        <p className="App-intro">Press Play to play instrument sound.</p>
-        <p><button onClick={this.playTestInstrument.bind(this)}>Play</button></p>
-        <MIDISounds ref={(ref) => (this.midiSounds = ref)} appElementName="root" instruments={[3]} />	
+      <div className="App">
+       
+        <p className="App-intro">Select instrument and press Play.</p>		
+		<p><select value={this.state.selectedInstrument} onChange={this.onSelectInstrument.bind(this)}>{this.createSelectItems()}</select></p>
+		<p><button onClick={this.playTestInstrument.bind(this)} disabled={!this.state.cached}>Play</button></p>
+		<p>Component</p>
+		<MIDISounds ref={(ref) => (this.midiSounds = ref)} appElementName="root" instruments={[4]} />	
+		<hr/>
+		<p>Sources: <a href={'https://www.npmjs.com/package/midi-sounds-react'}>https://www.npmjs.com/package/midi-sounds-react</a></p>
       </div>
     );
   }
 }
- 
+
 export default MidiButton;
