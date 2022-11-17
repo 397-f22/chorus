e1ec4e83c9f8d5e3f3c7057ec244277fbba42d69
OwenJanssen <owenjanssen2023@u.northwestern.edu>
2022-11-17 15:01:06 -0600

    Rows For Each Drum

--- src/components/PlayNote.jsx
+++ src/components/PlayNote.jsx
@@ -4,7 +4,24 @@ import { emptyBeatArray } from "../utilities/test.js"
 
 export const PlayNote = ({bpm}) => {
 	const [loop, setLoop] = useState(emptyBeatArray);
-	const instruments = [4, 318, 387, 258, 619, 628, 816]
+	const instruments = {
+		"piano": 4,
+		"electric guitar": 318,
+		"electic bass": 387,
+		"acoustic guitar": 258,
+		"trumpet": 619,
+		"trombone": 628,
+		"shakuhaki": 816
+	};
+	
+	const drums = {
+		"Bass drum": 3,
+		"Rimshot": 11,
+		"Snare drum": 27,
+		"Hand clap": 22,
+		"Hi-hat": 35
+	};
+
 	const mappings = {
 		"a": 60,
 		"s": 61,
@@ -27,32 +44,36 @@ export const PlayNote = ({bpm}) => {
 		midiSounds.stopPlayLoop();
 	}
 
-	const updateLoop = (i) => {
+	const updateDrumLoop = (i, drum) => {
 		var loopCopy = [...loop];
-		if (loopCopy[i][0].length > 0){
-			loopCopy[i][0] = [];
+		if (loopCopy[i][0].length > 0 && loopCopy[i][0].includes(drum)){
+			loopCopy[i][0] = loopCopy[i][0].filter((x) => x !== drum);
 		} else {
-			loopCopy[i][0] = [27];
+			loopCopy[i][0] = [...loopCopy[i][0], drum];
 		}
 		setLoop(loopCopy);
 	}
 
-	const isSelected = (i) => {
-		return loop[i][0].length > 0;
+	const isSelected = (i, drum) => {
+		return loop[i][0].length > 0 && loop[i][0].includes(drum);
 	}
 
-	return <div>
-		<div>Drum:</div>
-		<div className="row">
-			{loop.map((beat, i) => {
-				return <button className={isSelected(i) ? "selected" : "unselected"} key={i} onClick={() => {
-					updateLoop(i);
-				}}>{i}</button>
-			})}
-		</div>
+	return <div style={{marginLeft: "20px", marginTop: "20px"}}>
+		{	
+			Object.keys(drums).map(drum => {
+				return <div className="row">
+					<div className="instrument-label">{drum}:</div>
+					{loop.map((beat, i) => {
+						return <button className={isSelected(i, drums[drum]) ? "selected" : "unselected"} key={i} onClick={() => {
+							updateDrumLoop(i, drums[drum]);
+						}}></button>
+					})}
+				</div>
+			})
+		}
 		<button onClick={playTestInstrument}>playLoop</button>
 		<button onClick={stopLoop}>stopLoop</button>
-		<MIDISounds ref={(ref) => (setMidiSounds(ref))} drums={[27]}/>
+		<MIDISounds ref={(ref) => (setMidiSounds(ref))} drums={Object.values(drums)}/>
 	</div>	
 };
 
