389d0bcb088a2c406dc47023614241e427e4775d
ekohrt <e.a.kohrt@gmail.com>
2022-11-17 15:43:35 -0600

    begin note selection bar

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 import PlayNote from './components/PlayNote';
 import BpmSelector from './components/BpmSelector';
+import {NoteSelectorBar} from './components/NoteSelectorBar.jsx'
 
 const App = () => {
   const [bpm, setBpm] = useState(120);
+  const [note, setNote] = useState(60);
 
   return (
     <div className='KeyListener'>
@@ -18,7 +20,9 @@ const App = () => {
         </div>
 
         <PlayNote bpm={bpm}/>
+        <NoteSelectorBar note={note} setNote={setNote}></NoteSelectorBar>
   		</div>
+      
     </div>
   );
 };
---
+++ src/components/NoteSelectorBar.jsx
@@ -0,0 +1,11 @@
+import {note as note_name} from "midi-note" 
+// distinct notes - make a button for each of these 
+// conversion chart: https://miro.medium.com/proxy/1*CDXHKG0-4QO9Y-DCTAcqPg.png
+const notes = [57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68]
+
+export const NoteSelectorBar = ({note, setNote}) => {
+
+    return <h1>Hello {note_name(69)}</h1>
+}
+
+
