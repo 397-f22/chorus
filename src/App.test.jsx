6494687ef13ad4f09217c7412bfbb00905e1623a
OwenJanssen <owenjanssen2023@u.northwestern.edu>
2022-11-23 15:46:01 -0600

    Testing Setup

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,10 +1,12 @@
-import {describe, expect, test} from 'vitest';
+import {describe, expect, test, it} from 'vitest';
 import {fireEvent, render, screen} from '@testing-library/react';
+import { printBeats } from './utilities/printBeats';
 import App from './App';
 
-describe('counter tests', () => {
-  test("App Starts", () => {
-    render(<App />);
-    expect(screen.getByText('chorus')).toBeDefined();
+describe('function tests', () => {
+  it('printBeats works', async () => {
+    const beats = [[[],[]],[[1],[]],[[],[[1,[1],1]]],[[1],[[1,[1],1]]],[[1,2],[[1,[1],1],[2,[2],2]]]];
+    console.log(printBeats(beats))
+    expect(printBeats(beats)==="[[[],[]],[[1],[]],[[],[[1,[1],1]]],[[1],[[1,[1],1]]],[[1,2],[[1,[1],1],[2,[2],2]]]]").toBeTruthy();
   });
 });
