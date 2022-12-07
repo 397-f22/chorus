b920d2273b5bfcf58fb9fcc54a17a0998fe4bdb9
Cynthia <Liangshao2023@u.northwestern.edu>
2022-12-06 18:06:40 -0600

    add coverage test

--- src/components/BpmSelector.shao.test.jsx
+++ src/components/BpmSelector.shao.test.jsx
@@ -4,13 +4,8 @@ import { describe, expect, test, it } from 'vitest';
 import { fireEvent, render, screen } from '@testing-library/react';
 
 describe('bpm selector test', () => {
-    it('when bpm is set to 120, it show 120 in input', () => {
+    it('when bpm is set to 110, it show 110 in input', () => {
         render(<BpmSelector bpm={110} setBpm={() => {}} id={0} note={0}/>);
         expect(screen.findByText(/110/i));
     });
-
-    // it('when c# is the selected note, c# is selected', () => {
-    //     render(<NoteSelectorBar note={1} setNote={() => {}} octave={5} setOctave={() => {}}/>);
-    //     expect(screen.getByRole('1-selected')).toBeTruthy();
-    // });
 });
